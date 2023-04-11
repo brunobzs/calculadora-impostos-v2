5d27d08aebd8667ecdc8163450281608f6cd82b0
Bruno Carvalho <brunobzs@gmail.com>
2023-04-11 17:28:33 -0300

    Adiciona novas features

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 const tipoPessoaRadios = document.querySelectorAll('input[name="tipoPessoa"]');
 const tipoJuridicaFields = document.querySelectorAll('.tipo-juridica');
 
-function toggleTipoJuridicaFields() {
-    tipoJuridicaFields.forEach(field => {
-        field.style.display = (tipoPessoaRadios[1].checked) ? 'block' : 'none';
-    });
-}
+// function toggleTipoJuridicaFields() {
+//     tipoJuridicaFields.forEach(field => {
+//         field.style.display = (tipoPessoaRadios[1].checked) ? 'block' : 'none';
+//     });
+// }
+//
+// toggleTipoJuridicaFields();
+//
+// tipoPessoaRadios.forEach(radio => {
+//     radio.addEventListener('change', () => {
+//         toggleTipoJuridicaFields();
+//     });
+// });
 
-toggleTipoJuridicaFields();
-
-tipoPessoaRadios.forEach(radio => {
-    radio.addEventListener('change', () => {
-        toggleTipoJuridicaFields();
+$(document).ready(function () {
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').toggleClass('show');
     });
 });
